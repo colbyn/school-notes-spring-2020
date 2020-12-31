39899af267c8bfdaa7f8b5f3704acd046e7f9e31
Colbyn Wadman <hello@colbyn.com>
2020-12-31 12:09:13 -0700

    auto save

---
+++ sample/stock-photos/readme.md
@@ -0,0 +1,1 @@
+All stock photos are from `Pexels.com`.
---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+set -e
+
+rm -rf docs
+rm -rf output-release
+rm -rf output
+
+subscript compile \
+    --manifest subscript.toml \
+    --output-dir output-release \
+    --base-url https://colbyn.github.io/school-notes-spring-2020/
+
+mv output-release docs
+
+# git add docs
+# git commit -m "update site"
+# git push
+
